407e653ce73c1420bb9acf6f9e6dbc2db69f7de2
Danva <danva@arrowhitech.co>
2023-08-08 14:43:55 +0700

    done 1

--- testsite/members/models.py
+++ testsite/members/models.py
@@ -6,15 +6,15 @@ from django.urls import reverse
 class Category(models.Model):
     title = models.CharField(max_length=255)    
     slug = models.SlugField(unique=True, max_length=255)
-    content = models.TextField()
+    image_url = models.TextField()
 
 class Product(models.Model):
     name = models.CharField(max_length=255)        
     slug = models.SlugField(unique=True, max_length=255)
-    price = models.SlugField(unique=True, max_length=255)
+    price = models.SlugField(max_length=255)
     description = models.TextField()
 
 class Customer(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
-    phone = models.CharField(max_length=11)
+    email = models.CharField(max_length=255)
